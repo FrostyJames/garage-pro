c53fb3c4e129bfc572dbcd6330e7dbc1344eff47
james <ivanlavan773@gmail.com>
2025-08-26 10:27:24 +0300

    started working on the models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Date, Text, Float
 from sqlalchemy.orm import relationship
 from database import Base
 
+
 class Customer(Base):
     __tablename__ = 'customers'
     id = Column(Integer, primary_key=True)
