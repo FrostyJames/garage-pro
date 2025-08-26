706621bf128af7816c30db32115825238e9e1980
james <ivanlavan773@gmail.com>
2025-08-26 10:29:29 +0300

    updated code for class Service record

--- models.py
+++ models.py
@@ -31,10 +31,13 @@ class Vehicle(Base):
 
 class ServiceRecord(Base):
     __tablename__ = 'service_records'
+
     id = Column(Integer, primary_key=True)
     vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
     service_type = Column(String)
     date = Column(Date)
     notes = Column(Text)
     cost = Column(Float)
+
     vehicle = relationship("Vehicle", back_populates="service_records")
+
